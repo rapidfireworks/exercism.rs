caacf8c0c0652b694e2873a8a264a755443e0629
rapidfireworks <rapidfireworks@gmail.com>
2021-03-08 19:16:16 +0900

    Solve

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -1,3 +1,20 @@
 pub fn build_proverb(list: &[&str]) -> String {
-    unimplemented!("build a proverb from this list of items: {:?}", list)
+  if list.is_empty() {
+    String::from("")
+  } else {
+    let end_index = list.len() - 1;
+    list
+      .iter()
+      .zip(list.iter().cycle().skip(1))
+      .enumerate()
+      .map(|(index, (&curr, &next))| {
+        if index < end_index {
+          format!("For want of a {} the {} was lost.", curr, next)
+        } else {
+          format!("And all for the want of a {}.", next)
+        }
+      })
+      .collect::<Vec<String>>()
+      .join("\n")
+  }
 }
