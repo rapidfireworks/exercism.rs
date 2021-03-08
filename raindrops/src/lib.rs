58d0d761f9cc86b9611bf9084af8718ca41b92af
rapidfireworks <rapidfireworks@gmail.com>
2021-03-08 10:29:21 +0900

    Solve

--- raindrops/src/lib.rs
+++ raindrops/src/lib.rs
@@ -1,3 +1,25 @@
 pub fn raindrops(n: u32) -> String {
-    unimplemented!("what sound does Raindrop #{} make?", n)
+  let sounds = sounds(&n);
+  if sounds.is_empty() {
+    n.to_string()
+  } else {
+    sounds
+  }
+}
+
+fn sound(n: &u32) -> &str {
+  match n {
+    3 => "Pling",
+    5 => "Plang",
+    7 => "Plong",
+    _ => "",
+  }
+}
+
+fn sounds(n: &u32) -> String {
+  [3, 5, 7]
+    .iter()
+    .filter(|&d| n % d == 0)
+    .map(sound)
+    .collect()
 }
