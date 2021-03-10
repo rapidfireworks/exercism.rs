e8e38c39776db2e771fe22c2b8202ca70628edc6
rapidfireworks <rapidfireworks@gmail.com>
2021-03-10 09:00:04 +0900

    Refactor

--- proverb/src/lib.rs
+++ proverb/src/lib.rs
@@ -5,8 +5,6 @@ pub fn build_proverb(list: &[&str]) -> String {
     .map(|&s| format!("And all for the want of a {}.", s));
 
   list
-    .iter()
-    .as_slice()
     .windows(2)
     .map(|w| format!("For want of a {} the {} was lost.", w[0], w[1]))
     .chain(last)
