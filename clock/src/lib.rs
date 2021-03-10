62be73b57b879e1800cc48ea336f49981dd5f2d3
rapidfireworks <rapidfireworks@gmail.com>
2021-03-10 09:56:34 +0900

    Solve

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,15 +1,44 @@
-pub struct Clock;
+use std::fmt::{Debug, Display};
+
+use chrono::{self, Duration, NaiveTime, Timelike};
+
+pub struct Clock {
+  time: NaiveTime,
+}
 
 impl Clock {
-    pub fn new(hours: i32, minutes: i32) -> Self {
-        unimplemented!(
-            "Construct a new Clock from {} hours and {} minutes",
-            hours,
-            minutes
-        );
+  pub fn new(hours: i32, minutes: i32) -> Self {
+    let day = 24 * 60 * 60;
+    let mut secs = ((hours * 60 + minutes) * 60) % day;
+    if secs < 0 {
+      secs += day;
     }
+    Clock {
+      time: NaiveTime::from_num_seconds_from_midnight(secs as u32, 0),
+    }
+  }
 
-    pub fn add_minutes(&self, minutes: i32) -> Self {
-        unimplemented!("Add {} minutes to existing Clock time", minutes);
+  pub fn add_minutes(&self, minutes: i32) -> Self {
+    Clock {
+      time: self.time + Duration::minutes(minutes.into()),
     }
+  }
+}
+
+impl Display for Clock {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    write!(f, "{:02}:{:02}", self.time.hour(), self.time.minute())
+  }
+}
+
+impl Debug for Clock {
+  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+    write!(f, "{:02}:{:02}", self.time.hour(), self.time.minute())
+  }
+}
+
+impl PartialEq for Clock {
+  fn eq(&self, other: &Self) -> bool {
+    self.time == other.time
+  }
 }
