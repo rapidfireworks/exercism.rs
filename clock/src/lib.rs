add6dc7b3fd79081d349d5970d164d66cfbc2c10
rapidfireworks <rapidfireworks@gmail.com>
2021-03-10 10:27:51 +0900

    Refactor

--- clock/src/lib.rs
+++ clock/src/lib.rs
@@ -1,44 +1,28 @@
-use std::fmt::{Debug, Display};
+use std::fmt::Display;
 
-use chrono::{self, Duration, NaiveTime, Timelike};
+use chrono::{self, Duration, NaiveTime};
 
+#[derive(Debug, PartialEq)]
 pub struct Clock {
   time: NaiveTime,
 }
 
 impl Clock {
-  pub fn new(hours: i32, minutes: i32) -> Self {
-    let day = 24 * 60 * 60;
-    let mut secs = ((hours * 60 + minutes) * 60) % day;
-    if secs < 0 {
-      secs += day;
-    }
+  pub fn new(hours: i64, minutes: i64) -> Self {
     Clock {
-      time: NaiveTime::from_num_seconds_from_midnight(secs as u32, 0),
+      time: NaiveTime::from_hms(0, 0, 0) + Duration::hours(hours) + Duration::minutes(minutes),
     }
   }
 
-  pub fn add_minutes(&self, minutes: i32) -> Self {
+  pub fn add_minutes(&self, minutes: i64) -> Self {
     Clock {
-      time: self.time + Duration::minutes(minutes.into()),
+      time: self.time + Duration::minutes(minutes),
     }
   }
 }
 
 impl Display for Clock {
   fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-    write!(f, "{:02}:{:02}", self.time.hour(), self.time.minute())
-  }
-}
-
-impl Debug for Clock {
-  fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-    write!(f, "{:02}:{:02}", self.time.hour(), self.time.minute())
-  }
-}
-
-impl PartialEq for Clock {
-  fn eq(&self, other: &Self) -> bool {
-    self.time == other.time
+    write!(f, "{}", self.time.format("%H:%M").to_string())
   }
 }
