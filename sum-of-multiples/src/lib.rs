c56d7979f94e9b97588fcffc1e1315877361a503
rapidfireworks <rapidfireworks@gmail.com>
2021-03-16 10:47:01 +0900

    Solve

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,7 +1,12 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-    unimplemented!(
-        "Sum the multiples of all of {:?} which are less than {}",
-        factors,
-        limit
-    )
+  let factors: Vec<&u32> = factors.iter().filter(|&&x| 0 < x).collect();
+  (1..limit).filter(|x| is_multiple(x, &factors)).sum()
+}
+
+fn is_multiple(dividend: &u32, divisors: &[&u32]) -> bool {
+  divisors
+    .iter()
+    .filter(|&&x| dividend % x == 0)
+    .next()
+    .is_some()
 }
