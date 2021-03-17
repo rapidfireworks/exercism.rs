bcafe9339be379805a8b03931ab92d9831db48a4
rapidfireworks <rapidfireworks@gmail.com>
2021-03-17 12:00:44 +0900

    Solve

--- anagram/src/lib.rs
+++ anagram/src/lib.rs
@@ -1,9 +1,23 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
-pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&str]) -> HashSet<&'a str> {
-    unimplemented!(
-        "For the '{}' word find anagrams among the following words: {:?}",
-        word,
-        possible_anagrams
-    );
+use unicode_segmentation::{self, UnicodeSegmentation};
+
+pub fn anagrams_for<'a>(word: &str, possible_anagrams: &[&'a str]) -> HashSet<&'a str> {
+  let word = word.to_lowercase();
+  possible_anagrams
+    .iter()
+    .filter(|&&possible| anagram(&word, &possible.to_lowercase()))
+    .map(|&x| x)
+    .collect()
+}
+
+fn anagram(lhs: &str, rhs: &str) -> bool {
+  lhs != rhs && frequencies(lhs) == frequencies(rhs)
+}
+
+fn frequencies(word: &str) -> HashMap<&str, i64> {
+  word.graphemes(true).fold(HashMap::new(), |mut acc, val| {
+    *acc.entry(val).or_insert(0) += 1;
+    acc
+  })
 }
