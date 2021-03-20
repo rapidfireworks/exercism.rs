e167969ebb54610a16556126507cb9e50674d5d3
rapidfireworks <rapidfireworks@gmail.com>
2021-03-20 13:52:20 +0900

    Solve

--- space-age/src/lib.rs
+++ space-age/src/lib.rs
@@ -2,21 +2,20 @@
 // In order to pass the tests you can add-to or change any of this code.
 
 #[derive(Debug)]
-pub struct Duration;
+pub struct Duration {
+  years: f64,
+}
 
 impl From<u64> for Duration {
-    fn from(s: u64) -> Self {
-        unimplemented!("s, measured in seconds: {}", s)
+  fn from(s: u64) -> Self {
+    Duration {
+      years: s as f64 / 31_557_600.0,
     }
+  }
 }
 
 pub trait Planet {
-    fn years_during(d: &Duration) -> f64 {
-        unimplemented!(
-            "convert a duration ({:?}) to the number of years on this planet for that duration",
-            d,
-        );
-    }
+  fn years_during(d: &Duration) -> f64;
 }
 
 pub struct Mercury;
@@ -28,11 +27,43 @@ pub struct Saturn;
 pub struct Uranus;
 pub struct Neptune;
 
-impl Planet for Mercury {}
-impl Planet for Venus {}
-impl Planet for Earth {}
-impl Planet for Mars {}
-impl Planet for Jupiter {}
-impl Planet for Saturn {}
-impl Planet for Uranus {}
-impl Planet for Neptune {}
+impl Planet for Mercury {
+  fn years_during(d: &Duration) -> f64 {
+    d.years / 0.240_846_7
+  }
+}
+impl Planet for Venus {
+  fn years_during(d: &Duration) -> f64 {
+    d.years / 0.615_197_26
+  }
+}
+impl Planet for Earth {
+  fn years_during(d: &Duration) -> f64 {
+    d.years
+  }
+}
+impl Planet for Mars {
+  fn years_during(d: &Duration) -> f64 {
+    d.years / 1.880_815_8
+  }
+}
+impl Planet for Jupiter {
+  fn years_during(d: &Duration) -> f64 {
+    d.years / 11.862_615
+  }
+}
+impl Planet for Saturn {
+  fn years_during(d: &Duration) -> f64 {
+    d.years / 29.447_498
+  }
+}
+impl Planet for Uranus {
+  fn years_during(d: &Duration) -> f64 {
+    d.years / 84.016_846
+  }
+}
+impl Planet for Neptune {
+  fn years_during(d: &Duration) -> f64 {
+    d.years / 164.791_32
+  }
+}
