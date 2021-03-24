c1626da5b831fc0507683d8108dfa352dd949493
rapidfireworks <rapidfireworks@gmail.com>
2021-03-24 10:20:23 +0900

    Solve

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -1,3 +1,10 @@
 pub fn is_armstrong_number(num: u32) -> bool {
-    unimplemented!("true if {} is an armstrong number", num)
+  let len = (num as f64).log10() as u32 + 1;
+  let mut sum = 0;
+  let mut dividend = num;
+  for divisor in (0..len).map(|e| 10u32.pow(e)).rev() {
+    sum += (dividend / divisor).pow(len);
+    dividend %= divisor;
+  }
+  sum == num
 }
