94ede016d6f7d6c73b217b457c8189ba4c9fe4e7
rapidfireworks <rapidfireworks@gmail.com>
2021-03-24 09:50:47 +0900

    Improve performance comparing at index 0

--- sublist/src/lib.rs
+++ sublist/src/lib.rs
@@ -1,5 +1,3 @@
-use std::cmp::Ordering;
-
 #[derive(Debug, PartialEq)]
 pub enum Comparison {
   Equal,
@@ -9,14 +7,15 @@ pub enum Comparison {
 }
 
 pub fn sublist<T: PartialEq>(_first_list: &[T], _second_list: &[T]) -> Comparison {
+  use std::cmp::Ordering::*;
   match _first_list.len().cmp(&_second_list.len()) {
-    Ordering::Less if is_sublist(_first_list, _second_list) => Comparison::Sublist,
-    Ordering::Equal if _first_list == _second_list => Comparison::Equal,
-    Ordering::Greater if is_sublist(_second_list, _first_list) => Comparison::Superlist,
+    Less if is_sublist(_first_list, _second_list) => Comparison::Sublist,
+    Equal if _first_list == _second_list => Comparison::Equal,
+    Greater if is_sublist(_second_list, _first_list) => Comparison::Superlist,
     _ => Comparison::Unequal,
   }
 }
 
 fn is_sublist<T: PartialEq>(sub: &[T], sup: &[T]) -> bool {
-  (0..=(sup.len() - sub.len())).any(|i| sup[i..i + sub.len()].eq(sub))
+  sub.is_empty() || sup.windows(sub.len()).any(|w| w[0] == sub[0] && w.eq(sub))
 }
