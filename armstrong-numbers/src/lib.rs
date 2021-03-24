ae12f7ad3fbce5e6913272a1c4cb31c0795e7bb4
rapidfireworks <rapidfireworks@gmail.com>
2021-03-24 10:32:14 +0900

    Refactor

--- armstrong-numbers/src/lib.rs
+++ armstrong-numbers/src/lib.rs
@@ -1,10 +1,4 @@
 pub fn is_armstrong_number(num: u32) -> bool {
   let len = (num as f64).log10() as u32 + 1;
-  let mut sum = 0;
-  let mut dividend = num;
-  for divisor in (0..len).map(|e| 10u32.pow(e)).rev() {
-    sum += (dividend / divisor).pow(len);
-    dividend %= divisor;
-  }
-  sum == num
+  num == (0..len).map(|e| (num / 10u32.pow(e) % 10).pow(len)).sum()
 }
