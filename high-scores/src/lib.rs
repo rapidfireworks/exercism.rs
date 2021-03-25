b03974ba3d5f93132c6cb27d07bbfc774c23752d
rapidfireworks <rapidfireworks@gmail.com>
2021-03-25 11:49:51 +0900

    Solve

--- high-scores/src/lib.rs
+++ high-scores/src/lib.rs
@@ -1,27 +1,30 @@
 #[derive(Debug)]
-pub struct HighScores;
+pub struct HighScores {
+  scores: Vec<u32>,
+}
 
 impl HighScores {
-    pub fn new(scores: &[u32]) -> Self {
-        unimplemented!(
-            "Construct a HighScores struct, given the scores: {:?}",
-            scores
-        )
+  pub fn new(scores: &[u32]) -> Self {
+    HighScores {
+      scores: scores.iter().map(|&x| x).collect(),
     }
+  }
 
-    pub fn scores(&self) -> &[u32] {
-        unimplemented!("Return all the scores as a slice")
-    }
+  pub fn scores(&self) -> &[u32] {
+    &self.scores
+  }
 
-    pub fn latest(&self) -> Option<u32> {
-        unimplemented!("Return the latest (last) score")
-    }
+  pub fn latest(&self) -> Option<u32> {
+    self.scores.last().map(|&x| x)
+  }
 
-    pub fn personal_best(&self) -> Option<u32> {
-        unimplemented!("Return the highest score")
-    }
+  pub fn personal_best(&self) -> Option<u32> {
+    self.scores.iter().max().map(|&x| x)
+  }
 
-    pub fn personal_top_three(&self) -> Vec<u32> {
-        unimplemented!("Return 3 highest scores")
-    }
+  pub fn personal_top_three(&self) -> Vec<u32> {
+    let mut result: Vec<_> = self.scores.iter().map(|&x| x).collect();
+    result.sort_by(|lhs, rhs| rhs.cmp(lhs));
+    result.iter().take(3).map(|&x| x).collect()
+  }
 }
