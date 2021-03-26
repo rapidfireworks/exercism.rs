2337f437b9327b72cf46dae739e47fafd96c0b59
rapidfireworks <rapidfireworks@gmail.com>
2021-03-26 12:31:49 +0900

    Solve

--- matching-brackets/src/lib.rs
+++ matching-brackets/src/lib.rs
@@ -1,6 +1,14 @@
 pub fn brackets_are_balanced(string: &str) -> bool {
-    unimplemented!(
-        "Check if the string \"{}\" contains balanced brackets",
-        string
-    );
+  let mut stack: Vec<char> = vec![];
+  for bracket in string.chars() {
+    match bracket {
+      '(' | '{' | '[' => stack.push(bracket),
+      ')' | '}' | ']' => match (stack.pop(), bracket) {
+        (Some('('), ')') | (Some('{'), '}') | (Some('['), ']') => continue,
+        _ => return false,
+      },
+      _ => continue,
+    }
+  }
+  stack.is_empty()
 }
