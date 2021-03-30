35039b87d002eccf20afe4b15f778b394c001421
rapidfireworks <rapidfireworks@gmail.com>
2021-03-30 10:02:07 +0900

    Refactor removing references

--- sum-of-multiples/src/lib.rs
+++ sum-of-multiples/src/lib.rs
@@ -1,8 +1,8 @@
 pub fn sum_of_multiples(limit: u32, factors: &[u32]) -> u32 {
-  let factors: Vec<&u32> = factors.iter().filter(|&&x| 0 < x).collect();
-  (1..limit).filter(|x| is_multiple(x, &factors)).sum()
+  let fs: Vec<_> = factors.iter().filter(|&&f| 0 < f).copied().collect();
+  (1..limit).filter(|&m| is_multiple(m, &fs)).sum()
 }
 
-fn is_multiple(dividend: &u32, divisors: &[&u32]) -> bool {
-  divisors.iter().any(|&&x| dividend % x == 0)
+fn is_multiple(dividend: u32, divisors: &[u32]) -> bool {
+  divisors.iter().any(|divisor| dividend % divisor == 0)
 }
